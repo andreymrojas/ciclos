2d811269667784d2c1de2cb3f881dc8fdf317d8f
andrey rojas <andreymrojas668@gmail.com>
2022-06-25 09:41:19 -0500

    cambios

--- src/convertir.java
+++ src/convertir.java
@@ -1,2 +1,6 @@
 public class convertir {
-    {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/numeros_131313.java
@@ -0,0 +1,14 @@
+public class numeros_131313 {
+    public static void main (String [] args){
+        //imprimir 1 3 1 3 1 3
+        for (int i = 1; i <= 6; i++) {
+            int E;
+            if (i % 2 == 0) {
+                E = 3;
+            } else {
+                E = 1;
+            }
+            System.out.println("numero: " + E);
+        }
+    }
+}
